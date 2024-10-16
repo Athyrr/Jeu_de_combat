19e9307669c239fd49005df2c80a658671dedb30
Elliot <nedelliot84@gmail.com>
2024-10-16 16:29:42 +0200

    Add Sound Manager Previous commit : Update Program

--- SoundManager.cs
+++ SoundManager.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Media;
-using WMPLib;
-using System.IO;
+﻿using WMPLib;
 
 namespace Jeu_de_combat
 {
